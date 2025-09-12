4c45d162bca9d5673d59950155dc14136a0c539a
kennycaiguo <kennycai8888@outlook.com>
2025-09-11 19:12:39 -0600

    learn {{}} and v-text/v-html

---
+++ lesson3-data-binding/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p v-text="name"></p>
+    <p v-text="age"></p>
+    <p v-html="name"></p>
+    <p v-html="age"></p>
+  </div>
+</template>
+
+<script>
+  export  default {
+    data() {
+      return {
+        name: "Marycake",
+        age:"<span style='color:deeppink'>18</span>"
+      }
+    },
+  }
+
+</script>
+
+<style scoped>
+
+</style>
