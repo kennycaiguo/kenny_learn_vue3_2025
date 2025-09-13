48bbd2c46d8219bb6a39244060119536b016d3c0
kennycaiguo <kennycai8888@outlook.com>
2025-09-12 18:52:29 -0600

    event-binding demo

--- lesson5-event-binding/src/App.vue
+++ lesson5-event-binding/src/App.vue
@@ -1,12 +1,26 @@
 <template>
   <div>
-
+     <div class="box">{{counter}}</div>
+     <button @click="increment">increase</button>
+     <button @click="decrement">decrease</button>
   </div>
 </template>
 
 <script>
   export  default {
-
+    data() {
+      return {
+        counter: 0
+      }
+    },
+    methods:{ //有快捷键vmethod
+      increment(){
+        this.counter++;
+      },
+      decrement(){
+        this.counter--;
+      }
+    }
   }
 
 </script>
