986363d77c15d610caef8fd20b135e31306ce87f
kennycaiguo <kennycai8888@outlook.com>
2025-09-12 21:43:26 -0600

    event

---
+++ lesson6-$event/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <button @click="test1">test1</button>
+    <button @click="test2('abc')">test2</button>
+    <button @click="test3('event:',$event)">test3</button>
+    <button @click="test4($event)">test4</button>
+  </div>
+</template>
+
+<script>
+  export  default {
+    data() {
+      return {
+        msg: "hello"
+      }
+    },
+    methods: {
+      test1(e) {
+        alert(e.target.innerHTML)
+      },
+      test2(e) {
+        alert(e)
+      },
+      test3(msg,e) {
+        alert(msg + "---" + e.target.tagName)
+        console.log(e)
+      },
+      test4(e){
+        // alert(e.target.tagName)
+        console.log(event)
+      }
+    },
+  }
+
+</script>
+
+<style scoped>
+
+</style>
