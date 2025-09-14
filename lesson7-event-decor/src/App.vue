6fd63585b3895d42f841cd4b364b599edb97b956
kennycaiguo <kennycai8888@outlook.com>
2025-09-13 18:47:40 -0600

    event decoration

---
+++ lesson7-event-decor/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <div class="outer" @click.stop="fun1">
+      <div class="middle" @click.stop="fun2">
+        <div class="inner" @click.stop="fun3">
+
+        </div>
+      </div>
+    </div>
+    <a href="https://www.baidu.com/" @click.prevent="fun4">百度搜索</a>
+  </div>
+</template>
+
+<script>
+  export  default {
+    data() {
+      return {
+        msg: "Hello"
+      }
+    },
+    methods:{
+      fun1(){
+        console.log("outer的单击事件被触发")
+      },
+      fun2(){
+        console.log("middle的单击事件被触发")
+      },
+
+      fun3(e){
+        console.log("inner的单击事件被触发")
+        // e.stopPropagation()
+      },
+      fun4(e){
+        // alert("阻止超链接的跳转行为")
+        // e.preventDefault()
+      }
+    }
+  }
+
+</script>
+
+<style scoped>
+   .outer{
+     width: 300px;
+     height: 300px;
+     background-color: #999;
+   }
+
+   .middle{
+     height: 200px;
+     width: 200px;
+     background-color: lightsteelblue;
+   }
+
+   .inner{
+     width: 100px;
+     height: 100px;
+     background-color: lightcoral;
+   }
+</style>
