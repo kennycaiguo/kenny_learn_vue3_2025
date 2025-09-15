6c6211ef5fc9a2778ec0062d1d0fffa5f79c7af5
kennycaiguo <kennycai8888@outlook.com>
2025-09-15 16:15:32 -0600

    computed

---
+++ lesson9_computed/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h3>用计算属性实现字符串翻转</h3>
+    <label>
+      Enter a String: <input type="text" v-model="msg">
+    </label>
+    <p>Reserved: <span style="color: lightsteelblue;"> {{reversedMsg}}</span></p>
+  </div>
+
+</template>
+
+<script>
+  export  default {
+    data() {
+      return {
+        msg: " "
+      }
+    },
+    computed:{
+      reversedMsg(){
+        return this.msg.split("").reverse().join("")
+      }
+    }
+
+  }
+
+</script>
+
+<style scoped>
+
+</style>
