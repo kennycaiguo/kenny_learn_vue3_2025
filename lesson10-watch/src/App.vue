647b0bfc0d9f22116771f78a63d184984822048f
kennycaiguo <kennycai8888@outlook.com>
2025-09-15 16:56:59 -0600

    learn watcher

---
+++ lesson10-watch/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    姓氏: <input type="text" v-model="lastName">
+    名字: <input type="text" v-model="firstName">
+    <p>姓名:{{lastName}} {{firstName}}</p>
+    <p>姓名修改了{{num}}次</p>
+  </div>
+</template>
+
+<script>
+  export  default {
+    data() {
+      return {
+        firstName: "Tracy",
+        lastName: "Lou",
+        num:0
+      }
+    },
+    watch: {
+      firstName() {
+        this.num++
+      } ,
+      lastName() {
+        this.num++
+      }
+    },
+  }
+
+</script>
+
+<style scoped>
+
+</style>
