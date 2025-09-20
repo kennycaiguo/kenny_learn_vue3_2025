b21119c1f4b09924dd29d4b54273a1f7db7acd19
kennycaiguo <kennycai8888@outlook.com>
2025-09-19 18:37:52 -0600

    changed structure

---
+++ 02_Composition-api-projects/02_multi-comps/lesson17-custom-comp/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+     <MyComp />
+  </div>
+</template>
+
+<script setup>
+
+import MyComp from "./components/MyComp.vue";
+</script>
+
+<style scoped>
+
+</style>
---
+++ 02_Composition-api-projects/02_multi-comps/lesson17-custom-comp/src/components/MyComp.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="root">
+    <div class="title">
+      <h4>静夜思</h4>
+      <span class="btn" @click="flag=!flag">{{flag?"折叠":"展开"}}</span>
+    </div>
+    <div class="content" v-show="flag">
+      <h5>床前明月光</h5>
+      <h5>疑是地上霜</h5>
+      <h5>抬头望明月</h5>
+      <h5>低头思故乡</h5>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  import {ref} from "vue";
+  let flag = ref(true)
+</script>
+
+<style scoped>
+  .title{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border: 1px solid #ccc;
+    padding: 0 16px;
+  }
+
+  .title>h4{
+    margin: 0;
+    line-height: 32px;
+  }
+  .btn{
+    cursor: pointer;
+   }
+  .content{
+    border: 1px solid #ccc;
+    padding: 0 16px;
+  }
+</style>
