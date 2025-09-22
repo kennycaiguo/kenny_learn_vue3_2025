27340c32aaa51724a80bf900fa94ea16dc036ddc
kennycaiguo <kennycai8888@outlook.com>
2025-09-22 17:34:35 -0600

    data son to parent

---
+++ 02_Composition-api-projects/02_multi-comps/lesson19-data-son-to-dad/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+     <ChildComp @customEvent="handleChildEvn"/>
+  </div>
+</template>
+
+<script setup>
+import ChildComp from "./components/ChildComp.vue";
+let handleChildEvn = function (data) {
+  console.log("子组件传递过来的数据:" ,data)
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ 02_Composition-api-projects/02_multi-comps/lesson19-data-son-to-dad/src/components/ChildComp.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+     <button @click="sendData">click</button>
+  </div>
+</template>
+
+<script setup>
+// import {defineEmits} from "vue"; //defineEmits现在已经在编译器里面,不需要导入
+import {reactive} from "vue";
+
+let emits = defineEmits(["customEvent"])
+
+let worker = reactive({
+  name:"jack",
+  age:32,
+  gender:"Male"
+})
+let sendData = ()=>{
+  emits('customEvent',worker)
+}
+</script>
+
+<style scoped>
+
+</style>
