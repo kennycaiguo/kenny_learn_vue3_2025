089b5d63221040e2db5783159ae48616007b33b4
kennycaiguo <kennycai8888@outlook.com>
2025-09-22 12:49:35 -0600

    pass data: dad --> Child

---
+++ 02_Composition-api-projects/02_multi-comps/lesson18-component-comm-dad-to-son/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+     <ChildComp :msg="msg" :money="money" :user="user" :apples="apples"/>
+  </div>
+</template>
+
+<script setup>
+import {ref,reactive} from "vue";
+import ChildComp from "./components/ChildComp.vue";
+
+let msg = ref("孩子,爸爸给你点钱用用")
+let money = ref(99999999999)
+let user = reactive({
+  id:"u001",
+  name:"Jackline",
+  gender:"female",
+  age:18,
+  email:"Jackline123@gmail.com"
+})
+
+let apples = reactive(["红富士","加纳","黄元帅","花花牛"])
+</script>
+
+<style scoped>
+
+</style>
---
+++ 02_Composition-api-projects/02_multi-comps/lesson18-component-comm-dad-to-son/src/components/ChildComp.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <p>来自父亲组件的消息:{{msg}}</p>
+    <p>父亲给我的钱:{{money}}</p>
+    <div>
+      <h3>父亲给我的用户信息:</h3>
+      <p>Name:{{user.name}}, age:{{user.age}},gender:{{user.gender}},email:{{user.email}}</p>
+    </div>
+    <p>
+      父亲给我的苹果: <span v-for="apple in apples">{{apple}}--</span>
+    </p>
+  </div>
+</template>
+
+<script setup>
+  import {defineProps} from "vue";
+  //子组件接收父组件的数据,props会自动解包
+  let props = defineProps({
+      msg:String,
+      money:Number,
+      user:Object,
+      apples:Array
+  })
+
+</script>
+
+<style scoped>
+
+</style>
+
