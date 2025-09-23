425fdcb8e05fe21ae97d29e852c987c3af7c9b38
kennycaiguo <kennycai8888@outlook.com>
2025-09-23 14:45:02 -0600

    add scoped slot

---
+++ 02_Composition-api-projects/02_multi-comps/lesson22_2-scoped-slot/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <h3>渲染来自子组件的数据</h3>
+  <MyList>
+    <template #default="{acc}">
+        <div style="margin-bottom: 5px;">
+          <span>账户名称: {{acc.name}}</span>
+          <span>账户余额: {{acc.money}}</span>
+          <button @click="viewAccData(acc)">查看</button>
+        </div>
+
+    </template>
+  </MyList>
+</template>
+<script setup>
+import MyList from "./components/MyList.vue";
+let viewAccData = (data) => {
+  console.log(data)
+}
+</script>
+<style scoped>
+  span{
+    padding-right: 10px;
+    margin-right: 5px;
+  }
+  button{
+    padding: 3px 6px;
+  }
+</style>
---
+++ 02_Composition-api-projects/02_multi-comps/lesson22_2-scoped-slot/src/components/MyList.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul>
+    <li v-for="acc in accounts" :key="acc.number">
+      <slot :acc="acc"></slot>
+    </li>
+  </ul>
+</template>
+
+<script setup>
+import {reactive} from "vue";
+let accounts = reactive([
+  {number:1001001001,name:"Jack",money:10000000000},
+  {number:1001001002,name:"Brenda",money:10900000000},
+  {number:1001001003,name:"Mary",money:9900000000},
+])
+</script>
+
+<style scoped>
+  ul{
+    list-style-type: none;
+  }
+</style>
