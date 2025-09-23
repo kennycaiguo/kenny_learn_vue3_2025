baeaaaf8d4c0cd19064b9ec9670f4c391afdf39e
kennycaiguo <kennycai8888@outlook.com>
2025-09-23 13:53:58 -0600

    named slot

---
+++ 02_Composition-api-projects/02_multi-comps/lesson22-named-slot/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+     <BaseLayout>
+       <template #header>
+         这里是header
+       </template>
+       <template #default>
+         <div>
+           <h3>主体内容</h3>
+           <ul>
+             <li>1</li>
+             <li>2</li>
+             <li>3</li>
+             <li>4</li>
+             <li>5</li>
+           </ul>
+         </div>
+       </template>
+       <template #footer>
+         这里是footer
+       </template>
+     </BaseLayout>
+  </div>
+</template>
+
+<script setup>
+import BaseLayout from "./components/BaseLayout.vue";
+
+</script>
+
+<style scoped>
+
+</style>
