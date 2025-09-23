52c620b2f1daaed40e91da5f0ba24ca332a7df8f
kennycaiguo <kennycai8888@outlook.com>
2025-09-22 20:18:51 -0600

    init slot basic project

---
+++ 02_Composition-api-projects/02_multi-comps/lesson21-slot-basic/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+     <MyButton />
+  </div>
+</template>
+
+<script setup>
+import MyButton from "./components/MyButton.vue";
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ 02_Composition-api-projects/02_multi-comps/lesson21-slot-basic/src/components/MyButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <button>Click</button>
+</template>
+
+<script setup>
+
+
+</script>
+
+<style scoped>
+   button{
+     padding: 8px 15px;
+     border: none;
+     background: linear-gradient(to right,deeppink,mediumpurple);
+     border-radius: 5px;
+     color:white;
+   }
+</style>
