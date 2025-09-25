ca8ff2fd84fe34da5b49fb294b0828db8a24990c
kennycaiguo <kennycai8888@outlook.com>
2025-09-25 16:35:41 -0600

    message box example

---
+++ 02_Composition-api-projects/03_element-plus/lesson28-elementplus_messagebox/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <el-button plain @click="open">Click to open the Message Box</el-button>
+</template>
+
+<script  setup>
+import { ElMessage, ElMessageBox } from 'element-plus'
+
+const open = () => {
+  ElMessageBox.confirm(
+      '是否删除编号为xx的用户?',
+      '系统提示',
+      {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning',
+      }
+  )
+      .then(() => {
+        ElMessage({
+          type: 'success',
+          message: '删除用户成功',
+        })
+      })
+      .catch(() => {
+        ElMessage({
+          type: 'info',
+          message: '删除操作被取消了',
+        })
+      })
+}
+</script>
+
+<style scoped>
+
+</style>
