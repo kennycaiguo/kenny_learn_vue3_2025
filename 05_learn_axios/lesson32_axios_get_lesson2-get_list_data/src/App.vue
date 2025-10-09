0d6d03691337735c16d57588d679e28dfa96707e
kennycaiguo <kennycai8888@outlook.com>
2025-10-08 20:38:24 -0600

    el-table usage

--- 05_learn_axios/lesson32_axios_get_lesson2-get_list_data/src/App.vue
+++ 05_learn_axios/lesson32_axios_get_lesson2-get_list_data/src/App.vue
@@ -5,21 +5,30 @@
 	<!-- <ul v-if="len>0">
 	 	<li v-for="user in tdata">Id:{{user.id}} - username:{{user.username}} - Email:{{user.email}}</li>
 	 </ul> -->
-	 <el-table :data="tdata" style="width: 100%">
-	     <el-table-column fixed prop="id" label="ID" width="150" />
-	     <el-table-column prop="name" label="Name" width="120" />
-	     <el-table-column prop="username" label="userName" width="120" />
-	     <el-table-column prop="email" label="Email" width="120" />
-	     <el-table-column prop="address.street" label="Address" width="600" />
+	 <el-table :data="tableData" style="width: 100%">
+		 <!-- 
+		      fixed表示这个列是固定的,不会滚动
+			  prop:需要显示的属性名称,prop需要和数据对象的属性一一对应
+			  label:列标题 
+		  -->
+	     <el-table-column fixed prop="id" label="ID" width="150" /> <!—fixed表示这个列是固定的,不会滚动-->
+	     <el-table-column prop="name" label="Name" /> <!—没有fixed表示这个列不是固定的,会滚动-->
+	     <el-table-column prop="username" label="userName"/>
+	     <el-table-column prop="email" label="Email" />
+	     <el-table-column prop="address" label="Address"  />
 	     <el-table-column fixed="right" label="Operations" min-width="120">
 	       <template #default>
-	         <el-button link type="primary" size="small" @click="handleClick">
+	         <el-button link type="primary" size="small" @click="showDetail">
 	           Detail
 	         </el-button>
 	         <el-button link type="primary" size="small">Edit</el-button>
 	       </template>
 	     </el-table-column>
 	   </el-table>
+<!--	    {{tableData}} -->
+	  
+	
+	    
   </div>
 </template>
 
@@ -27,27 +36,36 @@
    import { ref,reactive ,computed} from 'vue';	
    import axios from 'axios';
    
-   let tdata = reactive([])
+   let tableData = reactive([])
    let len = ref(0)
    
-   let handleClick = async ()=>{
+   let handleClick = ()=>{
 	 
-			const options = {
-			  method: 'GET',
-			  url: 'https://jsonplaceholder.typicode.com/users'
-			};
-			
-			try {
-				const response = await axios.request(options);
-				// console.log(response.data);
-				tdata = response.data
-				console.log(tdata);
-				len.value = tdata.length
-			} catch (error) {
-				console.error(error);
-			}
-			
-			
+		// const options = {
+		//   method: 'GET',
+		//   url: 'https://jsonplaceholder.typicode.com/users'
+		// };
+		
+		 axios.get('https://jsonplaceholder.typicode.com/users')
+			  .then(res=>{
+				 // tableData = res.data  //不要使用=来直接赋值,否则可能不是响应式的数据,无法渲染.
+				 for (let item of res.data){
+					// console.log(item.address.street)
+					// console.log(item.address.suite)
+					// console.log(item.address.city)
+					//需要做地址拼接
+					let addr = item.address.city+","+item.address.street + ","+item.address.suite
+					item.address = addr
+					console.log(item.address);
+				    tableData.push(item)
+				 }
+				  console.log(tableData)
+			  })
+			  .catch(err=>{
+				  console.log(err);
+			  })
+			   
+						
    }
    
    let addData = async ()=>{
@@ -94,6 +112,10 @@
 	   })
 	     .then((response) => response.json())
 	     .then((json) => console.log(json));
+   }
+   
+   let showDetail = ()=>{
+	   
    }
 </script>
 
